6d6b13d2a3b4084785fe9914a7f003a22e96ac42
trackingdown <herpaderpsherbert@hotmail.com>
2015-02-19 13:46:36 -0500

    Finished pyramid-building functions
    Added in the 90 and 180 degree pyramid printing functions.

--- Lab 5/Lab 5/Source.cpp
+++ Lab 5/Lab 5/Source.cpp
@@ -12,54 +12,92 @@ using namespace std;
 
 string drawTri(int lines)
 {
-	//string pyramid;
-	int space = 0;
-	int asterix = 0;
+	string pyramid = "";
 
-	for (int i = 0; i < lines; i++)
+	for (int i = 1; i <= lines; i++)
 	{
-		//pyramid = lines - i;
-		for (int i= 0;i < lines; i++)
+		int spaces = lines - i;
+		int asterix = 2*i - 1;
+		for (int i = 1; i <= spaces; i++)
 		{
-			cout << " ";
+			pyramid += " ";
 		}
-
-
+		for (int i = 1; i <= asterix; i++)
+		{
+			pyramid += "*";
+		}
+    pyramid += "\n";
 	}
-
+  return pyramid;
 }
 
 string drawTriR90(int lines)
 {
+	string pyramid = "";
+	for (int i = 1; i <= lines; i++)
+	{
+		int asterix = 2*i-i;
+		for (int i = 1; i <= asterix; i++)
+		{
+			pyramid+= "*";
+		}
+	pyramid += "\n";
+	}
 
+	
+	for (int i = 1; i <= lines; i++)
+	{
+		int asterix = lines - i;
+		
+		for (int i = 1; i <= asterix; i++)
+		{
+			pyramid += "*";
+		}
+		pyramid += "\n";
+	}
+	
+	return pyramid;
 }
-
 string drawTriR180(int lines)
 {
-
+	string pyramid = "";
+	for (int i = lines; i > 0; i--)
+	{
+		int asterix = 2*i - 1;
+		int spaces = lines - i;
+		for (int i = 1; i <= spaces; i++)
+		{
+			pyramid += " ";
+		}
+		for (int i = 1; i <= asterix; i++)
+		{
+			pyramid += "*";
+		}
+    pyramid += "\n";
+	}
+  return pyramid;
 }
 
 
 
 int main()
 {
-	//n is used to the formula 2*n-1 to caluclate triangle size
-	int n;
-	while (n != "q" && n != "Q" && n <= 1 && n >= 20);
-	{
+	int input;
+	//while (input != "q" && input != "Q" && input <= 1 && input >= 20);
+	//{
 		cout << "Enter the size of your triangle (interger in [1, 20]) \nType Q to quit the program:" << endl;
 		
 
-		cin >> n;
+		cin >> input;
 
-		cout << "The triangle with size " << n << " is: \n" << drawTri(n) << endl;
+		cout << "The triangle with size " << input << " is: \n" << drawTri(input) << endl;
 
-		cout << "The rotation for 90 degrees clockwise: \n" << drawTriR90(n) << endl;
+		cout << "The rotation for 90 degrees clockwise: \n" << drawTriR90(input) << endl;
+		
+		cout << "The rotation for 180 degress clockwise: \n" << drawTriR180(input) << endl;
+		
 
-		cout << "The rotation for 180 degress clockwise: \n" << drawTriR180(n) << endl;
-
-
-	}
+	//}
 	system("pause");
 	return 0;
 }
